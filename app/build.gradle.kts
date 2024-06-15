5115c0a5b0909201a6cc40a369d9bf7643288dbe
ZuYun <jonas.jzy@gmail.com>
2024-06-15 16:49:01 +0800

    🎉

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 //    alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.room) apply false
-    alias(libs.plugins.compose) apply false
+    alias(libs.plugins.compose.compiler) apply false
     id("io.github.5hmlA.android.compose")
 //    id("io.github.5hmlA.android")
     id("io.github.5hmlA.protobuf")
